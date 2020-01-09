babce0007251ef815291c5b5ecb293b8bbbc3fed
MohamedKhattab <mohamed.kh1244@gmail.com>
2020-01-09 10:30:44 +0200

    Cv

--- src/component/portfolio.js
+++ src/component/portfolio.js
@@ -4,6 +4,7 @@ import Bouncy from '../img/bouncy.png';
 import Ajy from '../img/ajy.png';
 import Canvas from '../img/canvas.png';
 import Prog from '../img/prog.png';
+import Ipda3 from '../img/ipda3.png';
 class Portfolio extends Component {
     render() {
         return (
@@ -21,22 +22,27 @@ class Portfolio extends Component {
                         </li>
                         <li className = 'col-md-6'>
                             <a href = 'https://canvassite.netlify.com' target = '_blank'>
-                                <img src = {Canvas} alt = 'Start Up'/>
+                                <img src = {Canvas} alt = 'Canvas'/>
                             </a>
                         </li>
                         <li className = 'col-md-6'>
                             <a href = 'https://ajy.netlify.com' target = '_blank'>
-                                <img src = {Ajy} alt = 'Start Up'/>
+                                <img src = {Ajy} alt = 'Ajy'/>
                             </a>
                         </li>
                         <li className = 'col-md-6'>
                             <a href = 'https://bouncysite.netlify.com' target = '_blank'>
-                                <img src = {Bouncy} alt = 'Start Up'/>
+                                <img src = {Bouncy} alt = 'Bouncy'/>
                             </a>
                         </li>
-                        <li className = 'col-md-12'>
+                        <li className = 'col-md-6'>
                             <a href = 'https://progsite.netlify.com' target = '_blank'>
-                                <img src = {Prog} alt = 'Start Up'/>
+                                <img src = {Prog} alt = 'Prog'/>
+                            </a>
+                        </li>
+                        <li className = 'col-md-6'>
+                            <a href = 'https://ipda3site.netlify.com' target = '_blank'>
+                                <img src = {Ipda3} alt = 'Ipda3'/>
                             </a>
                         </li>
                     </ul>
